121a096571445327d477f9f3b5fd8faaefac7207
bdrhn9 <bedirhan@api3.org>
2023-05-10 15:51:48 +0300

    Generate error responses in specific format

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,6 +1,6 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import AWS from "aws-sdk";
-import { evmAddressSchema, signedDataSchema } from "./types";
+import { SignedData, evmAddressSchema, signedDataSchema } from "./types";
 import { go, goSync } from "@api3/promise-utils";
 import { isNil } from "lodash";
 import { deriveBeaconId, recoverSignerAddress } from "./evm";
@@ -23,43 +23,46 @@ const headers = {
   "content-type": "application/json",
 };
 
+const generateErrorResponse = (
+  statusCode: number,
+  message: string,
+  detail?: string,
+  causing?: SignedData,
+): APIGatewayProxyResult => {
+  return { statusCode, headers, body: JSON.stringify({ message, detail, causing }) };
+};
+
 export const upsertData = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  if (isNil(event.body))
-    return { statusCode: 400, headers, body: JSON.stringify("invalid request, missing http body") };
+  if (isNil(event.body)) return generateErrorResponse(400, "Invalid request, http body is missing");
 
   const goJsonParseBody = goSync(() => JSON.parse(event.body as string));
-  if (!goJsonParseBody.success)
-    return { statusCode: 400, headers, body: JSON.stringify("invalid request, body must be in JSON") };
+  if (!goJsonParseBody.success) return generateErrorResponse(400, "Invalid request, body must be in JSON");
 
   const goValidateSchema = await go(() => signedDataSchema.parseAsync(goJsonParseBody.data));
   if (!goValidateSchema.success)
-    return { statusCode: 400, headers, body: JSON.stringify(`invalid request, ${goValidateSchema.error.message}`) };
+    return generateErrorResponse(
+      400,
+      "Invalid request, body must fit signed data schema",
+      goValidateSchema.error.message,
+    );
 
   const signedData = goValidateSchema.data;
 
   const goRecoverSigner = goSync(() => recoverSignerAddress(signedData));
   if (!goRecoverSigner.success)
-    return {
-      statusCode: 400,
-      headers,
-      body: JSON.stringify(`unable to recover signer address, ${goRecoverSigner.error.message}`),
-    };
+    return generateErrorResponse(400, "Unable to recover signer address", goRecoverSigner.error.message);
 
-  if (signedData.airnode !== goRecoverSigner.data)
-    return { statusCode: 400, headers, body: JSON.stringify(`signature is invalid`) };
+  if (signedData.airnode !== goRecoverSigner.data) return generateErrorResponse(400, "Signature is invalid");
 
   const goDeriveBeaconId = goSync(() => deriveBeaconId(signedData.airnode, signedData.templateId));
   if (!goDeriveBeaconId.success)
-    return {
-      statusCode: 400,
-      headers,
-      body: JSON.stringify(
-        `unable to derive beaconId by given airnode and templateId, ${goDeriveBeaconId.error.message}`,
-      ),
-    };
+    return generateErrorResponse(
+      400,
+      "Unable to derive beaconId by given airnode and templateId",
+      goDeriveBeaconId.error.message,
+    );
 
-  if (signedData.beaconId !== goDeriveBeaconId.data)
-    return { statusCode: 400, headers, body: JSON.stringify(`beaconId is invalid`) };
+  if (signedData.beaconId !== goDeriveBeaconId.data) return generateErrorResponse(400, "beaconId is invalid");
 
   const goReadDb = await go(() =>
     docClient
@@ -67,41 +70,29 @@ export const upsertData = async (event: APIGatewayProxyEvent): Promise<APIGatewa
       .promise(),
   );
   if (!goReadDb.success)
-    return {
-      statusCode: 500,
-      headers,
-      body: JSON.stringify(`unable to get signed data from dynamodb to validate timestamp, ${goReadDb.error.message}`),
-    };
+    return generateErrorResponse(
+      500,
+      "Unable to get signed data from database to validate timestamp",
+      goReadDb.error.message,
+    );
 
   if (!isNil(goReadDb.data.Item) && parseInt(signedData.timestamp) <= parseInt(goReadDb.data.Item.timestamp))
-    return { statusCode: 400, headers, body: JSON.stringify(`request isn't updating the timestamp`) };
+    return generateErrorResponse(400, "Request isn't updating the timestamp");
 
   const goWriteDb = await go(() => docClient.put({ TableName: tableName, Item: signedData }).promise());
   if (!goWriteDb.success)
-    return {
-      statusCode: 500,
-      headers,
-      body: JSON.stringify(`unable to send signed data to dynamodb, ${goWriteDb.error.message}`),
-    };
+    return generateErrorResponse(500, "Unable to send signed data to database", goWriteDb.error.message);
 
   return { statusCode: 201, headers, body: JSON.stringify(signedData) };
 };
 
 export const getData = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   if (isNil(event.pathParameters?.airnode))
-    return {
-      statusCode: 400,
-      headers,
-      body: JSON.stringify("invalid request, missing path parameter airnode address"),
-    };
+    return generateErrorResponse(400, "Invalid request, path parameter airnode address is missing");
 
   const goValidateSchema = await go(() => evmAddressSchema.parseAsync(event.pathParameters?.airnode));
   if (!goValidateSchema.success)
-    return {
-      statusCode: 400,
-      headers,
-      body: JSON.stringify(`invalid request, path parameter needs to be an EVM address`),
-    };
+    return generateErrorResponse(400, "Invalid request, path parameter must be an EVM address");
 
   const goReadDb = await go(() =>
     docClient
@@ -115,19 +106,14 @@ export const getData = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
       .promise(),
   );
   if (!goReadDb.success)
-    return {
-      statusCode: 500,
-      headers,
-      body: JSON.stringify(`unable to get signed data from dynamodb, ${goReadDb.error.message}`),
-    };
+    return generateErrorResponse(500, "Unable to get signed data from database", goReadDb.error.message);
 
   return { statusCode: 200, headers, body: JSON.stringify({ count: goReadDb.data.Count, data: goReadDb.data.Items }) };
 };
 
 export const listData = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const goScanDb = await go(() => docClient.scan({ TableName: tableName }).promise());
-  if (!goScanDb.success)
-    return { statusCode: 500, headers, body: JSON.stringify(`unable to scan dynamodb, ${goScanDb.error.message}`) };
+  if (!goScanDb.success) return generateErrorResponse(500, "Unable to scan database", goScanDb.error.message);
 
   return { statusCode: 200, headers, body: JSON.stringify({ count: goScanDb.data.Count, data: goScanDb.data.Items }) };
 };
