cc45d13d16ed55d74b3dd1b5c002859800748024
bdrhn9 <bedirhan@api3.org>
2023-06-22 18:28:52 +0300

    Add CORS headers to allow all origins

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,7 @@
 export const MAX_BATCH_SIZE = parseInt(process.env.MAX_BATCH_SIZE as string);
 
 export const COMMON_HEADERS = {
-    "content-type": "application/json",
-  };
+  'content-type': 'application/json',
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': '*'
+};
